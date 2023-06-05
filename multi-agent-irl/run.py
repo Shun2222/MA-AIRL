f1bd36d444b7dcccf2b294ed86a18c180a04182a
shun <shun.su.jp@gmail.com>
2023-06-05 20:31:12 +0000

    data

--- multi-agent-irl/run.py
+++ multi-agent-irl/run.py
@@ -1,4 +1,4 @@
 import os 
 
 for i in range(10, 40):
-    os.system(f"python -m irl.mack.run_mack_aairl --env simple_path_finding_jaciii --expert_path ./data/path2/checkpoint01100-1000tra.pkl --seed {i} --logdir ./data/path2")
+    os.system(f"python -m irl.mack.run_mack_airl --env simple_path_finding_jaciii --expert_path ./data/path2/checkpoint01100-1000tra.pkl --seed {i} --logdir ./data/path2")
