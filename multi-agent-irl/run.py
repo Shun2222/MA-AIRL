20951f56abb658c9d94ae49d12311b6089adfd62
shun <shun.su.jp@gmail.com>
2023-06-06 19:32:35 +0900

    fix

--- multi-agent-irl/run.py
+++ multi-agent-irl/run.py
@@ -1,9 +1,4 @@
 import os 
 
-<<<<<<< HEAD
 for i in range(10, 11):
     os.system(f"python -m irl.mack.run_mack_aairl --env simple_path_finding_jaciii --expert_path ./data/path2/checkpoint01100-1000tra.pkl --seed {i} --logdir ./data/path2")
-=======
-for i in range(10, 40):
-    os.system(f"python -m irl.mack.run_mack_gail --env simple_path_finding_jaciii --expert_path ./data/path2/checkpoint01100-1000tra.pkl --seed {i} --logdir ./data/path2")
->>>>>>> 76a6a4cd48163aaacbed81f1e3969150eb44fcd0
