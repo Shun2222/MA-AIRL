cd2a1d5dae9ede0dbdb1ca3c9ed257677fc3fb52
shun2222 <shun.su.jp@gmail.com>
2023-10-06 17:28:37 +0000

    push data

--- multi-agent-irl/run.py
+++ multi-agent-irl/run.py
@@ -1,6 +1,6 @@
 import os 
 
-for i in range(20, 40):
+for i in range(26, 40):
     os.system(rf"python3 -m irl.mack.run_mack_airl \
                  --env simple_path_finding_jaciii \
                  --expert_path data/aamas/aamas_expert-1000tra.pkl \
